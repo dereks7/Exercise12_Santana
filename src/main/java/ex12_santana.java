72ef8e30bc038865a246256633049f977ee35761
dereks7 <90410123+dereks7@users.noreply.github.com>
2021-09-12 18:07:13 -0400

    Initial commit

---
+++ src/main/java/ex12_santana.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+Enter the principal: 1500
+Enter the rate of interest: 4.3
+Enter the number of years: 4
+After 4 years at 4.3%, the investment will be worth $1758.
+A = P(1 + rt)
+ */
+
+public class ex12_santana
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Enter the principal: ");
+        Scanner in1 = new Scanner(System.in);
+        String prin = in1.nextLine();
+        int n1 = Integer.parseInt(prin);
+        System.out.println("Enter the rate of interest: ");
+        Scanner in2 = new Scanner(System.in);
+        String roi = in2.nextLine();
+        double n2 = Double.parseDouble(roi);
+        System.out.println("Enter the number of years: ");
+        Scanner in3 = new Scanner(System.in);
+        String numyr = in2.nextLine();
+        double n3 = Double.parseDouble(numyr);
+        double rate = (n2/100);
+        double worth = n1*(1+(rate*n3));
+        System.out.println("After "+ n3 + " years at " + n2 + "% the investment will be worth $"+ worth +".");
+
+    }
+}
